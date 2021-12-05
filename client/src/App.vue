92c52c9132c0e0d14c10cb525f7c941993bbe8a7
funkoke <michaelyenoke@gmail.com>
2021-12-05 18:51:17 +0800

    update UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <v-app>
+    
+    <!--toolbar-->
+    <!--API: https://vuetifyjs.com/en/api/v-app-bar/-->    
     <v-app-bar
       app
       color="primary"
       dark
     >
+
+
       <div class="d-flex align-center">
+        <!--
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
@@ -23,10 +29,14 @@
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
         />
+        -->
+
+        
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer>tabtracker</v-spacer>
 
+      <!--
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
@@ -35,12 +45,53 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
+      --> 
     </v-app-bar>
 
+    <div class="space">
+      <p>jhkhkhk</p>
+    </div>
+    <!-- Floating With Search
+    https://vuetifyjs.com/en/components/toolbars/#floating-with-search
+    -->
+    <!--
+    <v-card
+    class="pa-4"
+    flat
+    height="300px"
+    img="https://cdn.vuetifyjs.com/images/toolbar/map.jpg"
+  >
+    <v-toolbar
+      dense
+      floating
+    >
+      <v-text-field
+        hide-details
+        prepend-icon="mdi-magnify"
+        single-line
+      ></v-text-field>
+
+      <v-btn icon>
+        <v-icon>mdi-crosshairs-gps</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+  -->
+
+    <!--vue-router -->
     <v-main>
       <router-view/>
     </v-main>
   </v-app>
+
+
+
+
+
 </template>
 
 <script>
@@ -53,3 +104,11 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+.space{
+    height: 300;
+    width: 300;
+    background-color: chocolate;
+}
+</style>
--- client/src/views/RegisterPage.vue
+++ client/src/views/RegisterPage.vue
@@ -1,6 +1,142 @@
 <template>
+  <v-card max-width="450" class="mx-auto">
+    <!--上半部的bar-->
+    <v-toolbar color="cyan" dark>
+      <!--三條線圖示
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      -->
+
+      <v-toolbar-title>Register</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <!--放大鏡圖示
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      -->
+    </v-toolbar>
+
+    <!--  List 的部分 -->
+    <v-list three-line>
+      <template>
+      <!--
+      <template v-for="(item, index) in items">
+        <-次標題:請輸入->
+        <v-subheader
+          v-if="item.header"
+          :key="item.header"
+          v-text="item.header"
+        ></v-subheader>
+        -->
+
+        <!--分隔線
+        <v-divider
+          v-else-if="item.divider"
+          :key="index"
+          :inset="item.inset"
+        ></v-divider>
+        -->
+
+        <!--list-->
+        <!--
+        <v-list-item
+          v-else
+          :key="item.title"
+        >
+        -->
+
+        <!--list頭像-->
+        <!--
+         <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+         -->
+        <!--list內容-->
+        <!--
+          <v-list-item-content>
+            <v-list-item-title v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          </v-list-item-content>
+          -->
+        <!--  
+        </v-list-item>
+        -->
+
+        <div class="container">
+          <v-layout>
+            <v-flex sm-12>
+              <panel title="Register">
+                <form name="tab-tracker-form" autocomplete="off">
+                  <v-text-field label="Email" v-model="email"></v-text-field>
+                  <br />
+                  <v-text-field
+                    label="Password"
+                    type="password"
+                    v-model="password"
+                    autocomplete="new-password"
+                  ></v-text-field>
+                </form>
+                <br />
+                <div class="danger-alert" v-html="error" />
+                <br />
+                <v-btn dark class="cyan" @click="register"> Register </v-btn>
+              </panel>
+            </v-flex>
+          </v-layout>
+        </div>
+
+        
+      </template>
+    </v-list>
+  </v-card>
+</template>
+<!--
+<script>
+export default {
+  data: () => ({
+    items: [
+      { header: "請輸入" },
+      
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Brunch this weekend?',
+          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'Oui oui',
+          subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          title: 'Birthday gift',
+          subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+          title: 'Recipe to try',
+          subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        },
+        
+    ],
+  }),
+};
+</script>
+-->
+<!--
+<template>
   <div class="container">
-    <v-layout custom>
+    <v-layout >
+
       <v-flex sm-12>
         <panel title="Register">
           <form name="tab-tracker-form" autocomplete="off">
@@ -19,9 +155,11 @@
           <v-btn dark class="cyan" @click="register"> Register </v-btn>
         </panel>
       </v-flex>
+    
     </v-layout>
   </div>
 </template>
+-->
 
 <script>
 import AuthenticationService from "@/services/AuthenticationService";
@@ -53,6 +191,9 @@ export default {
 <style scoped>
 .container {
   text-align: center;
+  border: red;
+  border-block-color: ;
+  background-color: chartreuse;
 }
 .error {
   color: red;
